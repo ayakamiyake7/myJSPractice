1fc2baf3595866b386326124595b39168d4e31bc
ayakamiyake <ayakamiyake7@gmail.com>
2021-11-09 17:05:59 +0900

    Add tab menu.

--- jsprogate/js/script.js
+++ jsprogate/js/script.js
@@ -1,16 +1,39 @@
 "use strict";
-{
-  //hamburger menu
-  const open = document.getElementById("open");
-  const overlay = document.querySelector(".overlay");
-  const close = document.getElementById("close");
 
-  open.addEventListener("click", () => {
-    overlay.classList.add("show");
-    open.classList.add("hide");
-  });
-  close.addEventListener("click", () => {
-    overlay.classList.remove("show");
-    open.classList.remove("hide");
-  });
-}
+// Tab menu
+// {
+//   const menuItems = document.querySelectorAll('.menu li a');
+//   const contents = document.querySelectorAll('.content');
+
+//   menuItems.forEach(clickedItem => {
+//     clickedItem.addEventListener('click', e => {
+//       e.preventDefault();
+
+//       menuItems.forEach(item => {
+//         item.classList.remove('active');
+//       });
+//       clickedItem.classList.add('active');
+
+//       contents.forEach(content => {
+//         content.classList.remove('active');
+//       });
+//       document.getElementById(clickedItem.dataset.id).classList.add('active');
+//     });
+//   });
+// }
+// {
+
+//hamburger menu
+//   const open = document.getElementById("open");
+//   const overlay = document.querySelector(".overlay");
+//   const close = document.getElementById("close");
+
+//   open.addEventListener("click", () => {
+//     overlay.classList.add("show");
+//     open.classList.add("hide");
+//   });
+//   close.addEventListener("click", () => {
+//     overlay.classList.remove("show");
+//     open.classList.remove("hide");
+//   });
+// }
